cff925597c6ee4d06db2c35dc177a0a8d6d9196b
aw-12-12 <fugushese@qq.com>
2026-03-15 15:53:32 +0800

    Post Process Highlight

--- Source/GAS_RPG/Public/Character/AuraEnemy.h
+++ Source/GAS_RPG/Public/Character/AuraEnemy.h
@@ -15,9 +15,10 @@ class GAS_RPG_API AAuraEnemy : public AAuraCharacterBase,public IEnemyInterface/
 {
 	GENERATED_BODY()
 public:
+	AAuraEnemy();
 	virtual void HighlightActor() override;
 	virtual void UnHighlightActor() override;
 	
-	UPROPERTY(BlueprintReadOnly)
-	bool bHighlighted = false;
+	//UPROPERTY(BlueprintReadOnly)
+	//bool bHighlighted = false;
 };
