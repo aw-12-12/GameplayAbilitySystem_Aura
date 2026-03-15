e95124b24297a3a1aab3a8aa366e26d948d72ea2
aw-12-12 <fugushese@qq.com>
2026-03-15 20:36:33 +0800

    Ability System Component and Attribute Set

---
+++ Source/GAS_RPG/Public/AbilitySystem/AuraAbilitySystemComponent.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "AuraAbilitySystemComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_RPG_API UAuraAbilitySystemComponent : public UAbilitySystemComponent
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/GAS_RPG/Public/AbilitySystem/AuraAttributeSet.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "AuraAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_RPG_API UAuraAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};
