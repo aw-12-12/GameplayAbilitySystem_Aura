6a57d1b72f3f77e34a986e5ecd669e664f53a410
aw-12-12 <fugushese@qq.com>
2026-03-17 16:48:46 +0800

    Health and Mana

--- Source/GAS_RPG/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/GAS_RPG/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -2,4 +2,50 @@
 
 
 #include "AbilitySystem/AuraAttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
 
+UAuraAttributeSet::UAuraAttributeSet()
+{
+	
+}
+
+
+//要让 RepNotify 生效，你还必须在属性集的 .cpp 文件底部注册它
+void UAuraAttributeSet::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	// 注册属性同步
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet,Health,COND_None,REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet,MaxHealth,COND_None,REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet,Mana,COND_None,REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet,MaxMana,COND_None,REPNOTIFY_Always);
+	
+	
+	
+}
+//REPNOTIFY_Always 的意思是：即使服务器发来的新值和客户端本地的值一样，也强制触发一次 OnRep。这在处理预测失败（Prediction Miss）回滚时非常有用
+
+
+void UAuraAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	//实现 OnRep 函数（必须调用 GAMEPLAYATTRIBUTE_REPNOTIFY 宏
+	//这个宏是 GAS 核心逻辑，用于通知 AbilitySystemComponent 属性已更改
+	// 它负责处理预测（Prediction）和内部状态更新
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet,Health,OldHealth);
+}
+
+void UAuraAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet,MaxHealth,OldMaxHealth);
+}
+
+void UAuraAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet,Mana,OldMana);
+}
+
+void UAuraAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet,MaxMana,OldMaxMana);
+}
--- Source/GAS_RPG/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/GAS_RPG/Public/AbilitySystem/AuraAttributeSet.h
@@ -13,5 +13,40 @@ UCLASS()
 class GAS_RPG_API UAuraAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+public:
+	UAuraAttributeSet();
+	
+	//同步注册变量必备,必须重写才能同步任何变量
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+	
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing = OnRep_Health,Category = "Vital Attributes")
+	FGameplayAttributeData Health;
+	
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing = OnRep_MaxHealth,Category = "Vital Attributes")
+	FGameplayAttributeData MaxHealth;
+	
+	UPROPERTY(BlueprintReadOnly,Replicatedusing = OnRep_Mana,Category = "Vital Attributes")
+	FGameplayAttributeData Mana;
+	
+	UPROPERTY(BlueprintReadOnly,Replicatedusing = OnRep_MaxMana,Category = "Vital Attributes")
+	FGameplayAttributeData MaxMana;
+	
+	UFUNCTION()//成为一个repnotify，必须是UFUNCTION【回调函数都必须是 UFUNCTION】
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;//可不带参数，也可以带一个必须是复制变量类型的参数，接受旧值有助于比较新旧值
+	//OnRep 的职责是响应变化（更新 UI、播放特效、通知其他系统），而不是产生变化。把函数设为 const 是一种强约束，防止你在同步回调里又去改属性，导致逻辑混乱甚至网络死循环。
+	
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+	
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
+	
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
 	
 };
+//当服务器上的一个变量发生了改变，并同步到客户端时，客户端会自动触发一个特定的回调函数。这个“变量同步 + 触发函数”的组合就叫 RepNotify
+
+//GEchange预测是GAS的内置功能
+//延迟补偿
+//属性是一个FGameplayAttributeData类型对象，存放在AS中，包含BaseValue,CurrentValue
