6915d80c2bff0d97f1c6d4161bf55b54818b3eca
aw-12-12 <fugushese@qq.com>
2026-03-18 08:48:29 +0800

    Aura User Widget and Widget Controller

---
+++ Source/GAS_RPG/Public/UI/Widgets/AuraUserWidget.h
@@ -0,0 +1,25 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "AuraUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_RPG_API UAuraUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable)
+	void SetWidgetController(UObject* InWidgetController);
+	UPROPERTY(BlueprintReadOnly) 
+	TObjectPtr<UObject> WidgetController; 
+protected:
+	UFUNCTION(BlueprintImplementableEvent)
+	void WidgetControllerSet();
+	
+};
