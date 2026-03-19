f3ff06b157949b56450a1aabf76bb9bb95dd3722
aw-12-12 <fugushese@qq.com>
2026-03-19 20:30:47 +0800

    Periodic Gameplay Effects

--- Source/GAS_RPG/Public/Actor/AuraEffectActor.h
+++ Source/GAS_RPG/Public/Actor/AuraEffectActor.h
@@ -25,6 +25,9 @@ protected:
 	UPROPERTY(EditAnywhere,BlueprintReadOnly,Category = "Applied Effects")
 	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
 		
+	UPROPERTY(EditAnywhere,BlueprintReadOnly,Category = "Applied Effects")
+	TSubclassOf<UGameplayEffect> DurationGameplayEffectClass;
+
 	UFUNCTION(BlueprintCallable)
 	void ApplyEffectToTarget(AActor* TargetActor,TSubclassOf<UGameplayEffect> GameplayEffectClass);
 
@@ -33,3 +36,4 @@ private:
 	
 };
 //绑定动态多播委托，回调函数OnOverlap的参数由OnComponentBeginOverlap的类型该用的特定委托的参数数量以及类型决定，不同类型的有不同类型的委托方法进入其中查看回调函数必备的参数列表，再根据参数创建对应参数的回调函数
+//spec:轻量版
